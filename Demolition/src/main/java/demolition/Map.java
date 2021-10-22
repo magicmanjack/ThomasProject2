0316e7e724510b3e99906ed881e97752005e0ab9
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2021-10-22 16:22:41 +1300

    Create Map.java

---
+++ Demolition/src/main/java/demolition/Map.java
@@ -0,0 +1,11 @@
+package demolition;
+
+import processing.core.PApplet;
+
+public class Map extends PApplet {
+	
+	public Map(String filename) {
+		String[] mapStrings = loadStrings(filename);
+	}
+
+}
