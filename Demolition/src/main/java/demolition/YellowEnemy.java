d496b77221f101f8c57245550fb292c7570901c9
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2021-10-28 23:45:48 +1300

    MAtrix magic

--- Demolition/src/main/java/demolition/YellowEnemy.java
+++ Demolition/src/main/java/demolition/YellowEnemy.java
@@ -7,7 +7,7 @@ public class YellowEnemy extends Entity {
 	public YellowEnemy(PApplet parent, int spawnX, int spawnY) {
 		x = spawnX;
 		y = spawnY;
-		xDir = 0;
+		xDir = -1;
 		yDir = 0;
 		animationStart = 0;
 		animationOffset = 0;
@@ -33,6 +33,30 @@ public class YellowEnemy extends Entity {
 	@Override
 	public void update() {
 		animate();
-		
+		if(deltaFrames % 60 == 0) {
+			if(collides()) {	
+				while(collides()) {
+					int[][] rot = {{0, -1},{1, 0}};
+					int[] vec = {xDir, yDir};
+					int[] vecOut = {0, 0};
+					for(int row = 0; row < rot.length; row++) {
+						vecOut[row] = vec[0] * rot[row][0] + vec[1] * rot[row][1];
+					}
+					xDir = vecOut[0];
+					yDir = vecOut[1];
+				}
+			}
+			x += xDir;
+			y += yDir;
+			if(xDir == -1 && yDir == 0) {
+				animationStart = 0;
+			} else if(xDir == 1 && yDir == 0) {
+				animationStart = 4;
+			} else if(xDir == 0 && yDir == -1) {
+				animationStart = 8;
+			} else if(xDir == 0 && yDir == 1) {
+				animationStart = 12;
+			}
+		}
 	}
 }
