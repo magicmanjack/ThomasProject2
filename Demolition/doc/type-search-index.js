1589661cbe9ff5e25b5095a010fa6a466af2c939
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2021-11-11 23:09:32 +1300

    Finished commenting the enemy class

--- Demolition/doc/type-search-index.js
+++ Demolition/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"demolition","l":"App"},{"p":"demolition","l":"Bomb"},{"p":"demolition","l":"Enemy"},{"p":"demolition","l":"Entity"},{"p":"demolition","l":"Map"},{"p":"demolition","l":"Player"},{"p":"demolition","l":"RedEnemy"},{"p":"demolition","l":"SampleTest"},{"p":"demolition","l":"YellowEnemy"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"demolition","l":"Enemy"}];updateSearchResults();
--- Demolition/src/main/java/demolition/Enemy.java
+++ Demolition/src/main/java/demolition/Enemy.java
@@ -3,13 +3,23 @@ package demolition;
 import processing.core.PApplet;
 import processing.core.PImage;
 
+/** The enemy class.
+ * Defines an enemy object which has a unique animation and collision detection.*/
 public class Enemy extends Entity {
+	
+	/** The y offset the sprite. 
+	 * This is the offset that the sprite is drawn relative to the position of the enemy.*/
 	public static final int SPRITE_OFFSET_Y = -16;
 	
+	/** The direction of the enemy.*/
 	public int xDir, yDir;
+	
+	/** Used for the the sprite animations.*/
 	public int animationStart, animationOffset, deltaFrames;
+	/** Contains the sprites.*/
 	public PImage[] sprites;
 	
+	/** Deals with the animation variables.*/
 	public void animate() {
 		deltaFrames++;
 		if(deltaFrames % 12 == 0) {
@@ -21,16 +31,20 @@ public class Enemy extends Entity {
 		}
 	}
 	
+	/** Returns true on collision.
+	 * If the Enemy collides with a solid or broken wall, this method will return true.*/
 	public boolean collides() {
 		return ((char)Map.maps[Map.currentLevel].mapStrings[y + yDir].charAt(x + xDir) == 'W' 
 				|| (char)Map.maps[Map.currentLevel].mapStrings[y + yDir].charAt(x + xDir) == 'B');
 	}
 	
+	/**Updates the Enemy.*/
 	@Override
 	public void update(PApplet parent) {
 		
 	}
 	
+	/**Draws the Enemy.*/
 	@Override
 	public void draw(PApplet parent) {
 		parent.image(sprites[animationStart + animationOffset], x * Map.TILE_WIDTH, y * Map.TILE_WIDTH + Player.SPRITE_OFFSET_Y + Map.Y_OFFSET);
